99edeec99a6604a551567e095640e54c3a835621
Tamilore <findtamilore@gmail.com>
2023-08-10 13:29:31 -0700

    u

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - prints "Programming is like building a multilingual puzzle
-*/
-int main (void)
+ * main - prints "Programming is like building a multilingual puzzle
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
