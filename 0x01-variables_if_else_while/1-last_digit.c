2abec8b8e25eecc1670eca4fd6b70e536b3756c9
Tamilore <findtamilore@gmail.com>
2023-08-11 12:00:17 -0700

    Task 1 update 3

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,12 +10,13 @@
 int main(void)
 {
 	int n;
+	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/* your code goes there */
-	int last_night = n % 10;
+	last_digiti = n % 10;
 
 	printf("Last digit of %d is %d ", n, last_digit);
 
