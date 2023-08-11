cf07cddb86ab28c5b159b2b1d5a5a1f129d29c0d
Tamilore <findtamilore@gmail.com>
2023-08-11 13:48:52 -0700

    task 101 updated

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,22 +7,16 @@ int main(void)
 {
 	int a, b, c;
 
-	for (a = 0; a < 8; a++)
-	{
-		for (b = 1; b < 9; b++)
-		{
-			if (a < b)
-			{
-				for (c = 2; c < 10; c++)
-				{
-					if (b < c)
-					{
+	for (a = 0; a < 8; a++) {
+		for (b = 1; b < 9; b++) {
+			if (a < b) {
+				for (c = 2; c < 10; c++) {
+					if (b < c) {
 						putchar(a + '0');
 						putchar(b + '0');
 						putchar(c + '0');
 
-						if (a != 7 || b != 8 || c != 9)
-						{
+						if (a != 7 || b != 8 || c != 9) {
 							putchar(',');
 							putchar(' ');
 						}
