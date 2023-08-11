f252e7900776d69c8eb188717db9ddcf8a3b46a5
Tamilore <findtamilore@gmail.com>
2023-08-11 11:42:33 -0700

    task 8 u

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,12 +5,15 @@
  */
 int main(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	for (i = 0; i < 10; i++)
 	{
 		putchar('0' + i);
 	}
 
-	for (char a = 'a'; a <= 'f'; a++)
+	char a;
+
+	for (a = 'a'; a <= 'f'; a++)
 	{
 		putchar(a);
 	}
