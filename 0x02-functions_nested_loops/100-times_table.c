b0f68536a95c25a185844dfd6a24ae24a37b834f
Tamilore <findtamilore@gmail.com>
2023-08-16 18:17:56 -0700

    task 13 re:

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,6 +35,7 @@ void print_times_table(int n)
 						_putchar(44);
 						_putchar(32);
 						_putchar(32);
+						_putchar(32);
 					}
 					_putchar(c + '0');
 				}
--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,17 +8,17 @@
 int main(void)
 {
 	int n1 = 1, n2 = 2;
-	int nextTerm = n1 + n2;
+	int nextTerm;
 	int i;
 
-	printf("1, 2, ");
+	printf("%d, %d, ", n1, n2);
 
-	for (i = 1; i <= 50; ++i)
+	for (i = 1; i <= 50; i++)
 	{
+		nextTerm = n1 + n2;
 		printf("%d, ", nextTerm);
 		n1 = n2;
 		n2 = nextTerm;
-		nextTerm = n1 + n2;
 	}
 
 	return (0);
