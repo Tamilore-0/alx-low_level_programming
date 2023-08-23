b2414422e5ea6d0b82e728b4929499dae87fd22b
Tamilore <findtamilore@gmail.com>
2023-08-23 07:39:21 -0700

    Task 1 done

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ *
+ * @dest: pointing to destination file
+ * @src: source
+ *
+ * Return: char
+ */
+char *_strcat(char *dest, char *src);
+{
+	int i = 0;
+	int j = 0;
+	char *ptr;
+
+	ptr = dest;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		src[j] = dest[i + j];
+		j++;
+	}
+
+	dest[i + j] = '\0';
+	return (ptr);
+}
