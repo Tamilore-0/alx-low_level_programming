e9eb6d427622f04272c411c63c89490c0b7ea7fb
Tamilore <findtamilore@gmail.com>
2023-08-23 08:33:54 -0700

    Task 1 and 2 done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@
  *
  * Return: char
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int j = 0;
@@ -20,7 +20,7 @@ char *_strcat(char *dest, char *src);
 	}
 	while (src[j] != '\0')
 	{
-		src[j] = dest[i + j];
+		dest[i + j] = src[j];
 		j++;
 	}
 
