faecbe50dcbb89dd7a1b2e53e016ba99a2fa2958
Tamilore <findtamilore@gmail.com>
2023-09-05 02:08:03 -0700

    task 2 and 3 reupload

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,35 +9,23 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0;
-	int len2 = 0;
-	int length;
-	int i;
-	int j;
+	int len1 = 0, len2 = 0;
+	int length, i, j
 	char *ptr;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	while (s1[len1] != '\0')
-	{
 		len1++;
-	}
 
 	while (s2[len2] != '\0')
-	{
 		len2++;
-	}
 
 	length = len1 + len2;
-
 	ptr = (char *) malloc((length + 1) * sizeof(char));
 
 	if (ptr == NULL)
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,4 +9,40 @@
  */
 int **alloc_grid(int width, int height)
 {
+	int i, j;
+	int **ptr;
 
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	ptr = malloc(height * sizeof(int));
+
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(width * sizeof(int));
+
+		if (ptr[i] == NULL)
+		{
+			for (i--; i >= 0; i--)
+				free(ptr[i]);
+			free(ptr);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+
+	return (ptr);
+}
