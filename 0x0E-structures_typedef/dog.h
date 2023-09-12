a162478b3c94546bdb950e23cb844af607fdcc96
Tamilore <findtamilore@gmail.com>
2023-09-12 13:53:05 +0100

    Corrected task 0-1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Entry point
@@ -12,3 +13,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
