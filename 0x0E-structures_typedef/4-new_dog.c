e1d90433bc96c247535db8fccb9d3c11deeb52c3
Tamilore <findtamilore@gmail.com>
2023-09-12 16:19:29 +0100

    Task 4 re: 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -50,14 +50,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
 	ptr = malloc(sizeof(dog_t));
 
 	if (ptr == NULL)
 	{
-		free(ptr);
 		return (NULL);
 	}
 
