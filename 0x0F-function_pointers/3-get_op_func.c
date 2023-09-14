228dcbe2cf769f70e33bf9f90dba350566bfe186
Tamilore <findtamilore@gmail.com>
2023-09-14 01:07:29 +0100

    Task 3 re-uploaded

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,9 +22,11 @@ int (*get_op_func(char *s))(int, int)
 	while (ops[i].op != NULL)
 	{
 		if (*s == *(ops[i].op) && *(s + 1) == '\0')
+		{
 			return (ops[i].f);
+		}
 		i++;
 	}
 	printf("Error\n");
-	return (NULL);
+	exit(98);
 }
