c1fa68a1ffeaa5d25fd1b2f79b6442146473f2c9
Tamilore <findtamilore@gmail.com>
2023-09-22 22:56:34 +0100

    Assembly code corrected

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,20 @@
-section .text
-	global main
+section .data                      ; This defines data that will be in memory
+    hello db "Hello, Holberton",10 ; 10 is the ASCII code for newline character
+    format db "%s",0              ; Format string for printf
+
+section .texti                   ; This section contains the program instructions
+    global main                  ;This line declares the main function as the entry point of the program. It's similar to the main function in a C program.
+
+    extern printf                ; Declare printf as an external symbol
 
 main:
-	mov	edx, len
-	mov	ecx, msg	;message variable
-	mov 	ebx, 1		;write to standard output
-	mov	eax, 4
-	int	0x80		;call kernel
+    sub rsp, 8                   ; Align the stack to 16 bytes
+    mov rdi, format              ; Load the format string address into rdi
+    mov rsi, hello               ; Load the address of the hello message into rsi
+    call printf                  ; Call the printf function
+    add rsp, 8                   ; Restore the stack pointer
 
-	mov	eax, 1 		;system call (sys_exit)
-	int	0x80		;call kernel
+    mov rax, 60                  ; Exit syscall number
+    xor rdi, rdi                 ; Exit status (0)
+    syscall                      ; Invoke syscall to exit
 
-section .data
-msg db 'Hello, Coding', 0xa	;message to print
-len equ $ - msg			;length of the string
