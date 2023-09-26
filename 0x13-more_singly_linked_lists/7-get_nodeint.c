365b7c09512e20c3b90bc0a87a8d1f751dd1fe6c
Tamilore <findtamilore@gmail.com>
2023-09-26 10:18:13 +0100

    Task 7 Upload

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Returns node at the given index in the linked list.
+ *
+ * @head: The head of the linked list.
+ * @index: The index of the node to return.
+ *
+ * Return: The node at the given index, or NULL if the index is out of bounds.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		if (count == index)
+			return (temp);
+
+		temp = temp->next;
+		count++;
+	}
+
+	return (NULL);
+}
