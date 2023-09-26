5a097ebae6cf7cac7031b963595c005dae65e231
Tamilore <findtamilore@gmail.com>
2023-09-26 10:20:41 +0100

    Task 7 Upload

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,15 +11,13 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-	listint_t *temp;
 
-	while (head)
+	while (head != NULL)
 	{
-		temp = head;
 		if (count == index)
-			return (temp);
+			return (head);
 
-		temp = temp->next;
+		head = head->next;
 		count++;
 	}
 
