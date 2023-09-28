9a9f4027b2f21a07c8873dd9d5a01a77f281019d
Tamilore <findtamilore@gmail.com>
2023-09-28 22:19:08 +0100

    Task 1-2 upload

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,23 +6,16 @@
  *
  * Return: void.
  */
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int binary[64];
-	int j, i = 0;
-
 	if (n == 0)
-		_putchar(0 + '0');
-
-	while (n > 0)
 	{
-		binary[i] = n & 1;
-		n = n >> 1;
-		i++;
+		_putchar('0');
 	}
-
-	for (j = i - 1; j >= 0; j--)
-		_putchar(binary[j] + '0');
-
+	else if (n == 1)
+	{
+		_putchar ('1');
+	}
+	print_binary(n >> 1);
+	_putchar('0' + (n & 1));
 }
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,7 +15,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (n == 0)
 		return (0);
 	if (index == 0)
-		return (0);
+		return (n & 1);
+	if (index > 63)
+		return (-1);
 
 	while (n > 0)
 	{
