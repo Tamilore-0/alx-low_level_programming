ab4b0d818a535aa75b0111e98f87a1f021fc049c
Tamilore <findtamilore@gmail.com>
2023-09-28 22:53:12 +0100

    Task 1-2 upload

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,12 +10,11 @@ void print_binary(unsigned long int n)
 {
 	if (n == 0)
 	{
-		_putchar('0');
+		_putchar('0' + 0);
+		return;
 	}
-	else if (n == 1)
-	{
-		_putchar ('1');
-	}
+
 	print_binary(n >> 1);
 	_putchar('0' + (n & 1));
 }
+
