f3d8959db157c465b40a8aea2cb5c5a4ce86907c
Tamilore <findtamilore@gmail.com>
2023-09-29 01:09:32 +0100

    task 100 updated

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -10,7 +10,5 @@ int get_endianness(void)
 	unsigned int num = 1;
 	char *byte = (char *)&num;
 
-	// If the first byte of the integer contains the least significant byte,
-	// return 1 (little-endian); otherwise, return 0 (big-endian).
 	return (*byte);
 }
