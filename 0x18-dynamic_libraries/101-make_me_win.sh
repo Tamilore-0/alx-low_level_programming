3b8b5e8c8ec82c601eefc14b409210549a9a7a13
Tamilore <findtamilore@gmail.com>
2023-11-13 19:58:29 +0100

    101-make_me_win.sh corrections

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "int rand() { return 5; }" > custom_lib.c
-gcc -shared -fPIC -o custom_lib.so custom_lib.c && LD_PRELOAD=./custom_lib.so ./gm 9 8 10 24 75 9
+gcc *.o -fPIC -shared -o libruin.so
+LD_PRELOAD=$PWD/libruin.so
