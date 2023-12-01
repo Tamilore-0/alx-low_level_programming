eb8560483cfe50ef7b27169bdd5f7ae6c87b9c20
Tamilore <findtamilore@gmail.com>
2023-12-01 15:27:54 +0100

    update

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,31 +4,27 @@ void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *current;
 	unsigned long int i;
-    unsigned char flag = 0;
+
+	int comma_flag = 0;
 
 	if (!ht)
 	{
-		printf("{}\n");
+		return;
 	}
 
 	printf("{");
-	for (i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size; ++i)
 	{
-		if (ht->array[i] != NULL)
-        {
-            if (flag == 1)
-                printf(", ");
+		current = ht->array[i];
+		while (current)
+		{
+			if (comma_flag)
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			comma_flag = 1;
 
-            current = ht->array[i];
-		    while (current != NULL)
-		    {
-			    printf("'%s': '%s'", current->key, current->value);
-			    current = current->next;
-                if (current != NULL)
-				    printf(", ");
-		    }
-            flag = 1;
-	    }
-    }
+			current = current->next;
+		}
+	}
 	printf("}\n");
 }
