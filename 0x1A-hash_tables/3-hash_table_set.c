9a2346f2144c44bd3c71cf9f7db61d9a8f4ee155
Tamilore <findtamilore@gmail.com>
2023-12-01 13:20:16 +0100

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,9 +1,42 @@
-#include "hash_tables.c"
+#include "hash_tables.h"
 
+/**
+ * hash_table_set - Adds an element to a hash table.
+ *
+ * @ht: A pointer to the hash table.
+ * @key: The key to be added.
+ * @value: The corresponding value to be associated with the key.
+ *
+ * Return: 1 on success, 0 on failure.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || value == NULL)
-        return (0);
+	hash_node_t *newnode = NULL;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	newnode = malloc(sizeof(hash_node_t));
+	if (newnode == NULL)
+		return (0);
+
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+
+	if (ht->array[index] == NULL)
+	{
+		newnode->next = ht->array[index];
+		ht->array[index] = newnode;
+	}
+	else
+	{
+		newnode->next = NULL;
+		ht->array[index] = newnode;
+	}
+
+	return (1);
 }
