e52e95ced7bb1d4661b959758b91c26a7fb7db72
Tamilore <findtamilore@gmail.com>
2023-12-01 13:30:57 +0100

    task 3 update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,24 +1,34 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - Adds an element to a hash table.
- *
- * @ht: A pointer to the hash table.
- * @key: The key to be added.
- * @value: The corresponding value to be associated with the key.
- *
- * Return: 1 on success, 0 on failure.
- */
+* hash_table_set - Adds an element to a hash table.
+*
+* @ht: A pointer to the hash table.
+* @key: The key to be added.
+* @value: The corresponding value to be associated with the key.
+*
+* Return: 1 on success, 0 on failure.
+*/
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 
 	hash_node_t *newnode = NULL;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
+
+	while (ht->array[index] != NULL)
+	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->value = strdup(value);
+			return (1);
+		}
+	}
 
 	newnode = malloc(sizeof(hash_node_t));
 	if (newnode == NULL)
@@ -27,7 +37,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	newnode->key = strdup(key);
 	newnode->value = strdup(value);
 
-	if (ht->array[index] == NULL)
+	if (ht->array[index] != NULL)
 	{
 		newnode->next = ht->array[index];
 		ht->array[index] = newnode;
