3aaffcb8c1792d2c129d584af95a3650e7412c97
tami-cp0 <findtamilore>
2024-03-12 18:18:59 +0100

    changed syntax v2

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -25,7 +25,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	while (low <= high)
 	{
-		pos = low + (value - array[low]) * (high - low) / (array[high] - array[low]);
+		pos = low + (((double)(high - low) /
+		(array[high] - array[low])) * (value - array[low]));
 
 		if (pos > size)
 		{
